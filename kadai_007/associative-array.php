ba582c24237ce5cbd5f2b48a0d9caf202f1a2e27
yuuyakk <141416428+yuuyakk@users.noreply.github.com>
2024-02-10 23:13:27 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  $print_r = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+  print_r($print_r);
+  ?>  
+</body>
+</html>
